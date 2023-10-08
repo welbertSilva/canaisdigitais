1ea2e09f1e04dd6c39d900060a0bb27f7eda3f19
Welbert josé da Silva <welbert.silva@gmail.com>
2023-10-08 19:40:28 -0300

    Adicionado menus ao header e redirecionameto para página de log in.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,11 +16,29 @@
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="antialiased">
+    <body class="antialiased scroll">
             <div class="navigation">           
                 <div class="item1"></div> 
                 <div class="item2">
                     <label> Canais Digitais</label>
+                </div>
+            </div>
+            <div class="item3">
+                <div>
+                    <nav class="navbar">
+                    <!-- LOGO -->
+                        <!-- NAVEGAÇÃO MENU -->
+                            <ul class="nav-links">
+                                <!-- NAVEGAÇÃO MENUS -->
+                                <div class="menu">
+                                    <li><a href="/">Visão Geral</a></li>
+                                    <li><a href="/">Caixa TEM</a></li>
+                                    <li><a href="/">Gerenciador Financeiro</a></li>
+                                    <li><a href="/">Internet Banking</a></li>
+                                    <li><a href="{{ route('login') }}">Acessar ADM</a></li>
+                                </div>
+                            </ul>
+                    </nav>    
                 </div>
             </div>
             @inertia
@@ -32,41 +50,101 @@
 <style type="text/css">
        
 /* This aligns items to the end line on main-axis */
+    html, body {
+    overflow: hidden;
+    }
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body {
+        font-family: Inter;
+    }
+    a {
+        text-decoration: none;
+    }
+    li {
+        list-style: none;
+    }
     .navigation {
-        height: 25rem;
-        width: 100%;
+        height: 8rem;
+        width: 99.8%;
         display:flex;
         flex-direction: row;
         justify-content:  flex-start ;
         align-items: center;
         background: #005CA9;
         gap: 2rem;
-        padding: 4rem 0rem 0rem 6rem;
+        padding: 4rem 2rem 0rem 4rem;
         margin: 0.2rem 0.0rem 0.0rem 0.2rem;
 
     }
     .item1 {
         background-image: url("../images/logotipo_negativo.svg");
-        background-size: 37rem;
+        background-size: 11rem;
         background-repeat: no-repeat, no-repeat;
-        height: 12rem;
-        width: 39rem;
+        height: 7rem;
+        width: 12rem;
     } 
     .item2 {
-        height: 16rem;
-        width: 120rem;
+        height: 10.5rem;
+        width: 15rem;
         margin-top:3.4rem;
     }
     .item2 label {
         color: #FFF;
         text-align: left;
         font-family: Inter;
-        font-size: 6rem;
+        font-size: 2rem;
         font-style: normal;
-        font-weight: 800;
+        font-weight: 700;
     }
 
+    .item2 a {
+        color: #FFF;
+        text-align: left;
+        justify-content:  flex-end ;
+        font-family: Inter;
+        font-size: 0.8rem;
+        font-style: normal;
+        font-weight: 400;
+    }
+    .item3{
+        width: 99.8%;
+        background: #005CA9;
+        display: flex;
+        align-content: flex-end;
+        flex-flow: column wrap;
+        height: 2.2rem;
+        margin: -2rem 0.0rem 0.0rem 0.2rem;
 
+    }
+/* NAVBAR ESTILO */
+.navbar {
+    display: flex;
+    align-items: left;
+    justify-content: space-between;
+    background-color: transparent;
+    color: #fff;
+   }
+   .nav-links a {
+    color: #fff;
+   }
+   /* NAVBAR MENU */
+   .menu {
+    display: flex;
+    gap: 1em;
+    font-size: 0.9rem;
+   }
+   .menu li:hover {
+    background-color: #4c9e9e;
+    border-radius: 5px;
+    transition: 0.3s ease;
+   }
+   .menu li {
+    padding: 5px 14px;
+   }
 /* Medium screens When on medium sized screens, we center it by evenly distributing empty space around items */
 @media (max-width: 1440px) {
     .navigation {
@@ -204,7 +282,5 @@
         font-weight: 300;
     }
 }
-
-
     
 </style>
