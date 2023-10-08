7abb6364f5e2daf312eba30d6b22cb4c42f9043d
Welbert josé da Silva <welbert.silva@gmail.com>
2023-10-08 18:03:17 -0300

    Adicionando header com responsividade

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -3,15 +3,17 @@ import { Link, Head } from '@inertiajs/react';
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
-            <Head title="Welcome" />
+            <Head title="GECDI" />
             <div className="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+               
+               
+                {/* <div className="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
                     {auth.user ? (
                         <Link
                             href={route('dashboard')}
                             className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                         >
-                            Dashboard
+                            Dashboardi
                         </Link>
                     ) : (
                         <>
@@ -30,10 +32,10 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                             </Link>
                         </>
                     )}
-                </div>
+                </div> */}
 
                 <div className="max-w-7xl mx-auto p-6 lg:p-8">
-                    <div className="flex justify-center">
+                    {/* <div className="flex justify-center">
                         <svg
                             viewBox="0 0 62 65"
                             fill="none"
@@ -45,9 +47,9 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                 fill="#FF2D20"
                             />
                         </svg>
-                    </div>
+                    </div> */}
 
-                    <div className="mt-16">
+                    {/* <div className="mt-16">
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                             <a
                                 href="https://laravel.com/docs"
@@ -71,7 +73,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     </div>
 
                                     <h2 className="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
-                                        Documentation
+                                        Documentação
                                     </h2>
 
                                     <p className="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
@@ -117,7 +119,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     </div>
 
                                     <h2 className="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
-                                        Laracasts
+                                        Laracasts blog
                                     </h2>
 
                                     <p className="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
@@ -288,9 +290,9 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
-                    <div className="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
+                    {/* <div className="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
                         <div className="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                             <div className="flex items-center gap-4">
                                 <a
@@ -318,11 +320,11 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                         <div className="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                             Laravel v{laravelVersion} (PHP v{phpVersion})
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
 
-            <style>{`
+            {/* <style>{`
                 .bg-dots-darker {
                     background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
                 }
@@ -331,7 +333,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                         background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
                     }
                 }
-            `}</style>
+            `}</style> */}
         </>
     );
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', '') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -16,7 +16,195 @@
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
-        @inertia
+    <body class="antialiased">
+            <div class="navigation">           
+                <div class="item1"></div> 
+                <div class="item2">
+                    <label> Canais Digitais</label>
+                </div>
+            </div>
+            @inertia
     </body>
 </html>
+
+<!-- <img src="../images/logotipo3x.png">
+                <label>Canais Digitais</label> -->
+<style type="text/css">
+       
+/* This aligns items to the end line on main-axis */
+    .navigation {
+        height: 25rem;
+        width: 100%;
+        display:flex;
+        flex-direction: row;
+        justify-content:  flex-start ;
+        align-items: center;
+        background: #005CA9;
+        gap: 2rem;
+        padding: 4rem 0rem 0rem 6rem;
+        margin: 0.2rem 0.0rem 0.0rem 0.2rem;
+
+    }
+    .item1 {
+        background-image: url("../images/logotipo_negativo.svg");
+        background-size: 37rem;
+        background-repeat: no-repeat, no-repeat;
+        height: 12rem;
+        width: 39rem;
+    } 
+    .item2 {
+        height: 16rem;
+        width: 120rem;
+        margin-top:3.4rem;
+    }
+    .item2 label {
+        color: #FFF;
+        text-align: left;
+        font-family: Inter;
+        font-size: 6rem;
+        font-style: normal;
+        font-weight: 800;
+    }
+
+
+/* Medium screens When on medium sized screens, we center it by evenly distributing empty space around items */
+@media (max-width: 1440px) {
+    .navigation {
+        height: 12rem;
+        width: 100%;
+        display:flex;
+        flex-direction: row;
+        justify-content:  flex-start ;
+        align-items: center;
+        background: #005CA9;
+        gap: 1rem;
+        padding: 2rem 0rem 0rem 6rem;
+        margin: 0.1rem 0.0rem 0.0rem 0.2rem;
+    }
+    .item1 {
+        background-image: url("../images/logotipo_negativo.svg");
+        background-size: 18rem;
+        background-repeat: no-repeat, no-repeat;
+        height: 6rem;
+        width: 18rem;
+    } 
+    .item2 {
+        height: 8rem;
+        width: 60rem;
+        margin-top:1.5rem;
+    }
+    .item2 label {
+        color: #FFF;
+        text-align: left;
+        font-family: Inter;
+        font-size: 3rem;
+        font-style: normal;
+        font-weight: 700;
+    }
+}
+@media (max-width: 1340px) {
+    .navigation {
+        height: 12rem;
+        width: 100%;
+        display:flex;
+        flex-direction: row;
+        justify-content:  flex-start ;
+        align-items: center;
+        background: #005CA9;
+        gap: 1rem;
+        padding: 2rem 0rem 0rem 6rem;
+        margin: 0.1rem 0.0rem 0.0rem 0.2rem;
+    }
+    .item1 {
+        background-image: url("../images/logotipo_negativo.svg");
+        background-size: 13rem;
+        background-repeat: no-repeat, no-repeat;
+        height: 5rem;
+        width: 13rem;
+    } 
+    .item2 {
+        height: 8rem;
+        width: 60rem;
+        margin-top:3rem;
+    }
+    .item2 label {
+        color: #FFF;
+        text-align: left;
+        font-family: Inter;
+        font-size: 2rem;
+        font-style: normal;
+        font-weight: 400;
+    }
+}
+@media (max-width: 800px) {
+    .navigation {
+        height: 5rem;
+        width: 100%;
+        display:flex;
+        flex-direction: row;
+        justify-content:  flex-start ;
+        align-items: center;
+        background: #005CA9;
+        gap: 0.2rem;
+        padding: 2rem 0rem 0rem 2rem;
+        margin: 0rem 0.0rem 0.0rem 0rem;
+    }
+    .item1 {
+        background-image: url("../images/logotipo_negativo.svg");
+        background-size: 6rem;
+        background-repeat: no-repeat, no-repeat;
+        height: 3rem;
+        width: 7rem;
+    } 
+    .item2 {
+        height: 5rem;
+        width: 40rem;
+        margin-top:1.7rem;
+    }
+    .item2 label {
+        color: #FFF;
+        text-align: left;
+        font-family: Inter;
+        font-size: 1rem;
+        font-style: normal;
+        font-weight: 300;
+    }
+}
+/* Small screens On small screens, we are no longer using row direction but column */
+@media (max-width: 665px) {
+    .navigation {
+        height: 7rem;
+        width: 100%; 
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        background: #005CA9;       
+    }
+    .item1 {
+        flex: 1 auto;
+        order: 1;
+        background-image: url("../images/logotipo_negativo.svg");
+        background-size: 4rem;
+        background-repeat: no-repeat, no-repeat;
+        height: 2rem;
+        width: 7rem;
+    } 
+    .item2 {
+        order: 2;
+        height:2rem;
+        width: 8rem;
+        
+    }
+    .item2 label {
+        color: #FFF;
+        text-align: left;
+        font-family: Inter;
+        font-size: 1rem;
+        font-style: normal;
+        font-weight: 300;
+    }
+}
+
+
+    
+</style>
